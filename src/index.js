f83c4051236edd79b637320a594a0a6af5c08941
Pavel <60615091+PavelHaradnik@users.noreply.github.com>
2020-03-20 10:56:58 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let rp = [];
+    for (let i=0; i<str.length; i++) {
+        for ( j=0; j<bracketsConfig.length; j++) {
+            if(str[i] === bracketsConfig [j] [0] && str[i] === bracketsConfig [j] [1]) {
+                if (rp[rp.length-1] === str [i]) {
+                    rp.pop();
+                } else rp.push (str[i]);
+                break;
+            }
+            if (str [i] === bracketsConfig [j] [0]) {
+                rp.push (str[i]);
+            }
+            if (str [i] === bracketsConfig [j] [1]) {
+                if (rp[rp.length-1] === bracketsConfig [j] [0]) {
+                    rp.pop();
+                }else return false
+            }
+        }
+    }
+    return rp.length === 0;
 }
+
